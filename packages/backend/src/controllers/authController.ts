f6ed996722385b66de26e7347b153a0fc2b63b9b
bynned <benny.cascarino@hotmail.com>
2024-10-02 16:35:01 +0300

    Check if the username already exists

--- packages/backend/src/controllers/authController.ts
+++ packages/backend/src/controllers/authController.ts
@@ -5,12 +5,20 @@ import User from '../models/User';
 export const register = async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
-  //Hash the password
-  const hashedPassword = await bcrypt.hash(password, 10);
-
   try {
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      res.status(400).json({ message: 'This username already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create and save the new user
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
+
     res.status(201).json({ message: 'User registered' });
   } catch (error) {
     res.status(500).json({ error: 'Error registering user' });

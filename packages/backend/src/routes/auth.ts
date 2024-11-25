baedacfb0928b3cf211985251d8bc6a6f2b3d596
bynned <benny.cascarino@hotmail.com>
2024-11-25 10:25:49 +0200

    remove useless router.post in auth.ts

--- packages/backend/src/routes/auth.ts
+++ packages/backend/src/routes/auth.ts
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/posts', createPost);
 router.get('/me', authenticateToken, getUserDetails); // Get user details
 
 export default router;
